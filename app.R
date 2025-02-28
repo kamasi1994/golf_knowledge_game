99f87a7b4879901335d118d5991040aeca6dd4c3
Conor Prescott <conorprescott15@gmail.com>
2025-02-28 12:50:15 +0000

    .

--- app.R
+++ app.R
@@ -288,7 +288,7 @@ ui <- dashboardPage(
       #################
       tabItem(
         tabName = "coin",
-        tags$img(src = "coinflip.jpg", width = "50px", height = "auto"),
+        tags$img(src = "coinflip.jpg", width = "50%", height = "auto"),
         h2("Are you feeling lucky?"),
         h4("Toss the coin to double your earnings for the previous week. If you lose, your earnings are set to €0 for that week"),
         h4("Only your first attempt is recorded. Any subsequent tosses are ignored"),

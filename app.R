f62cc9a76284e36fe2cce4f84f78f1d23afbc6c8
Conor Prescott <conorprescott15@gmail.com>
2025-04-07 15:03:48 +0000

    masters theme logo

--- app.R
+++ app.R
@@ -160,8 +160,9 @@ ui <- dashboardPage(
   # Dashboard header
   dashboardHeader(
     title = tags$span(
-      tags$img(src = "pif.png", height = "30px", style = "margin-right: 30px;"),
-      tags$img(src = "pga.png", height = "30px", style = "margin-right: 30px;")
+      # tags$img(src = "pif.png", height = "30px", style = "margin-right: 30px;"),
+      # tags$img(src = "pga.png", height = "30px", style = "margin-right: 30px;")
+        tags$img(src = "masters.jpg", height = "30px", style = "margin-right: 30px;")
   )),
   
   # Dashboard sidebar
